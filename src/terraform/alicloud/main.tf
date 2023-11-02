a450ad13f693002f4d1cf5948a39319ae5acbb5b
Mikhail Yurasov <myurasov@nvidia.com>
2023-11-02 15:47:59 -0700

    Temporarily change instance type to cheapest one

--- src/terraform/alicloud/main.tf
+++ src/terraform/alicloud/main.tf
@@ -21,10 +21,11 @@ module "isaac" {
   prefix         = "${var.prefix}-${var.deployment_name}-isaac"
   vswitch_netnum = 1
 
-  ssh_port       = var.ssh_port
-  isaac_enabled  = var.isaac_enabled
-  vpc            = module.common.vpc
-  key_pair       = module.common.key_pair
-  instance_type  = var.isaac_instance_type
+  ssh_port      = var.ssh_port
+  isaac_enabled = var.isaac_enabled
+  vpc           = module.common.vpc
+  key_pair      = module.common.key_pair
+  # instance_type  = var.isaac_instance_type
+  instance_type  = "ecs.g7a.large"
   resource_group = module.common.resource_group
 }
