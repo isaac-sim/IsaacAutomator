8cb67007204d8d56d8c0dad5cf2ee7b6d29cccc7
Mikhail Yurasov <myurasov@nvidia.com>
2023-11-03 04:56:07 -0700

    Add rules for custom ssh and nomachine

--- src/terraform/alicloud/ovkit/security.tf
+++ src/terraform/alicloud/ovkit/security.tf
@@ -5,36 +5,61 @@ resource "alicloud_security_group" "default" {
 
 # security rule for ssh
 resource "alicloud_security_group_rule" "allow_ssh" {
+  priority          = 1
+  security_group_id = alicloud_security_group.default.id
   type              = "ingress"
   ip_protocol       = "tcp"
   nic_type          = "intranet"
   policy            = "accept"
   port_range        = "22/22"
-  priority          = 1
-  security_group_id = alicloud_security_group.default.id
   cidr_ip           = "0.0.0.0/0"
 }
 
 # security rule for novnc
 resource "alicloud_security_group_rule" "allow_novnc" {
+  priority          = 2
+  security_group_id = alicloud_security_group.default.id
   type              = "ingress"
   ip_protocol       = "tcp"
   nic_type          = "intranet"
   policy            = "accept"
   port_range        = "6080/6080"
-  priority          = 2
-  security_group_id = alicloud_security_group.default.id
   cidr_ip           = "0.0.0.0/0"
 }
 
 # security rule for ping
 resource "alicloud_security_group_rule" "allow_ping" {
+  priority          = 3
+  security_group_id = alicloud_security_group.default.id
   type              = "ingress"
   ip_protocol       = "icmp"
   nic_type          = "intranet"
   policy            = "accept"
   port_range        = "-1/-1"
-  priority          = 3
+  cidr_ip           = "0.0.0.0/0"
+}
+
+# security rule for nomachine
+resource "alicloud_security_group_rule" "allow_nomachine" {
+  priority          = 4
   security_group_id = alicloud_security_group.default.id
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "4000/4000"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+# custom ssh port
+resource "alicloud_security_group_rule" "custom_ssh" {
+  priority          = 5
+  count             = var.ssh_port != 22 ? 1 : 0
+  security_group_id = alicloud_security_group.default.id
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "${var.ssh_port}/${var.ssh_port}"
   cidr_ip           = "0.0.0.0/0"
 }
