6d4413cbacedc524684d487b0c9a3f482e1583ff
Mikhail Yurasov <myurasov@nvidia.com>
2024-05-06 19:13:53 -0700

    Make sure display is available on app launch

--- uploads/autorun.sh
+++ uploads/autorun.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# This script is executed when
+# 1. the VM is first deployed
+# 2. the VM is started after being stopped
+
 # replace with your own command
 # for example:
 #  ~ubuntu/Desktop/isaacsim.sh --cmd="/isaac-sim/kit/kit /isaac-sim/apps/omni.isaac.sim.kit --allow-root"
