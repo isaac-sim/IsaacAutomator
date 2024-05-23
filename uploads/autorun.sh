e4211ceeeac0eca69be91288a1f0964db7e33977
cailani-nv <102393832+cailani-nv@users.noreply.github.com>
2024-05-22 21:58:26 -0500

    Update autorun.sh
    Signed-off-by: cailani-nv <102393832+cailani-nv@users.noreply.github.com>

--- uploads/autorun.sh
+++ uploads/autorun.sh
@@ -7,4 +7,4 @@
 # replace with your own command
 # for example:
 #  ~ubuntu/Desktop/isaacsim.sh --cmd="/isaac-sim/kit/kit /isaac-sim/apps/omni.isaac.sim.kit --allow-root"
-~ubuntu/Desktop/isaacsim.sh --cmd="bash uploads/pass-creds-to-container.sh ; apt-get update && apt-get install -y ffmpeg && /isaac-sim/kit/kit /isaac-sim/apps/omni.isaac.sim.kit --ext-folder /isaac-sim/apps --allow-root"
+~ubuntu/Desktop/isaacsim.sh --cmd="bash uploads/pass-creds-to-container.sh ; apt-get update && apt-get install -y vim ffmpeg && /isaac-sim/kit/kit /isaac-sim/apps/omni.isaac.sim.kit --ext-folder /isaac-sim/apps --allow-root"
