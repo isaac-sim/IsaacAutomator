fd23ef5a96d3072d4e07345994b1759b6eef7c27
Mikhail Yurasov <myurasov@nvidia.com>
2024-05-26 20:00:17 -0700

    Update autorun script

--- uploads/autorun.sh
+++ uploads/autorun.sh
@@ -6,5 +6,5 @@
 
 # replace with your own command
 # for example:
-#  ~ubuntu/Desktop/isaacsim.sh --cmd="/isaac-sim/kit/kit /isaac-sim/apps/omni.isaac.sim.kit --allow-root"
-~ubuntu/Desktop/isaacsim.sh
+#  ~/isaacsim.sh --cmd="/isaac-sim/kit/kit /isaac-sim/apps/omni.isaac.sim.kit --allow-root"
+~/isaacsim.sh
